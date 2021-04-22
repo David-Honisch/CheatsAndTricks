9f9ae74583ca50abd9b6b67fc45fa65962a9f54c
dh <david@honisch.org>
2021-04-22 21:10:14 +0200

    readme

--- create.dirs.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set inFile=%~dp0games.csv
-set outDir=%~dp0cheats
-set pattern=*.ct
-For /F "tokens=1 delims=;" %%a in (%inFile%) do (
-	echo mkdir "%outDir%%%~pa"
-	mkdir "%outDir%%%~pa"
-
-)
